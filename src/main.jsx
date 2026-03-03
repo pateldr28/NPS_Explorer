4d05cc2d32407dc73a9736e29eddefc4eef68b7d
savhannab <savhanna.t.l.preston@gmail.com>
2026-03-02 19:37:02 -0800

    initial

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+//Note: this is for example only feel free to change/remove 
+
+//Used for executing queries (and mutations)
+const queryClient = new QueryClient()
+
+//Client side routing
+const router = createBrowserRouter([
+    {
+        path: "/", 
+        //main component
+        Component: App,
+        children: [
+            {index: true, Component: Home },
+            {path: "tanstack/states/:stateId", Component: stateDetail},
+            {path: "tanstack/parks/:parkId", Component: parkDetail},
+        ]
+    }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router}/>
+        </QueryClientProvider>
+    </React.StrictMode>,
 )
