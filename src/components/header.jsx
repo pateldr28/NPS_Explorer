6f664eaa29d3b6e66f44d53e28a89003914c5b06
savhannab <savhanna.t.l.preston@gmail.com>
2026-03-11 20:15:31 -0700

    UI updates

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,16 +1,22 @@
+import OlympicNationalPark from "../images/OlympicNationalPark2.jpg";
 export default function Header() {
     return (
-        <header className="bg-[#F8F6EE] border-b border-stone-200">
-            <div className="max-w-6xl mx-auto px-6 py-8">
-                <h1 className="text-4xl font-semibold text-[#2F4F3A] mb-3">
-                    National Park Explorer
-                </h1>
-                <p className="text-stone-600 mb-6 max-w-2xl">
-                    Discover U.S. National Parks, explore parks by state,
-                    plan trips, and track the parks you've visited.
-                </p>
-                <input type="text" placeholder="Search parks" className="w-full max-w-md px-4 py-3
-                rounded-lg border border-stone-300 focus:outline-none focus:ring-2 focus:ring-[#2F4F3A]" />
+        <header className="border-b border-stone-200 bg-cover bg-center"
+            style={{ backgroundImage: `url(${OlympicNationalPark})` }}
+        >
+            <div className="bg-black/40">
+                <div className="max-w-6xl mx-auto px-6 py-8">
+                    <h1 className="text-4xl font-semibold text-white mb-3">
+                        National Park Explorer
+                    </h1>
+                    <p className="text-white mb-6 max-w-2xl">
+                        Discover U.S. National Parks, explore by state,
+                        plan trips, and track the parks you've visited.
+                    </p>
+                    <input type="text" placeholder="Search parks" className="w-full max-w-md px-5 py-3
+                    rounded-xl border border-white/60 focus:outline-none focus:ring-2 focus:ring-[#2F4F3A] bg-white/95
+                    backdrop-blur-sm" />
+                </div>
             </div>
         </header>
     )
