bd819f2cbfdbf5e841d7ec46b294ec96956fb47d
Max Baker <crimsonender99@gmail.com>
2026-03-14 16:59:16 -0700

    added store

---
+++ src/redux/store.js
@@ -0,0 +1,9 @@
+import {configureStore} from "@reduxjs/toolkit"
+
+const store = configureStore({
+    reducer:{
+        tracker:trakcerReducer
+    }
+})
+
+store.subscribe(()=>console.log(store.getState()))
