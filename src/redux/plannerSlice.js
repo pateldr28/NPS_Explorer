7b2cba6222fde5d088849002319f3734e391b0e3
Dristi Patel <dristi605@gmail.com>
2026-03-14 19:27:56 -0700

    Conected parks in parksInState component to the plannerSlice.

--- src/redux/plannerSlice.js
+++ src/redux/plannerSlice.js
@@ -6,26 +6,27 @@ export const plannerSlice = createSlice({
     reducers:{
         addParkToPlanner(state, action){
             //If the park is not already in the planner, than add it 
-            if(!state[action.payload.parkInfo.id]){
-                state[action.payload.parkInfo] = {
-                    id: action.payload.parkInfo.id, //might need for key
-                    parkCode: action.payload.parkInfo.parkCode, //to link park to its dedicated page (easier to get to all the info)
+            if(!state[action.payload.park.id]){
+                state[action.payload.park.id] = {
+                    id: action.payload.park.id, //might need for key
+                    parkName: action.payload.park.fullName, //name of the park 
+                    parkCode: action.payload.park.parkCode, //to link park to its dedicated page (easier to get to all the info)
                     notes: "", //empty notes intially
                     priority: 0, //0 priority initially 
                     startDate:"",
                     endDate:""
                 }
             }
-        },
-
-        updateParkInfoInPlanner(state, action){
-            //gets the park the user wants to update and changes the field based on that info
         }
+
+        // updateParkInfoInPlanner(state, action){
+        //     //gets the park the user wants to update and changes the field based on that info
+        // }
     }
 
 })
 
-export const selectPlannedParks = plannerSlice.selectSlice
+export const selectPlannedParks = (state) => state.plannedParks
 
 export const {addParkToPlanner} = plannerSlice.actions
 
