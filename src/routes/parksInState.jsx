d7038bd68835bbff2f6b58b6d34678be6447e0a5
Dristi Patel <dristi605@gmail.com>
2026-03-14 18:17:45 -0700

    Working on implementing planner portion. Changed states page to have add to planner button on card.

--- src/routes/parksInState.jsx
+++ src/routes/parksInState.jsx
@@ -14,20 +14,28 @@ export default function ParksInState() {
       </div>
       <div className="grid gap-8 md:grid-cols-2 xl:grid-cols-3">
           {parks?.data?.map((park) => (
-            <Link key={park.id} to={`/parks/${park.parkCode}`} 
-            className="bg-white rounded-2xl border border-stone-200 shadow-sm overflow-hidden hover:shadow-lg transition">
-            <img src={park.images?.[0]?.url} alt={park.images?.[0]?.altText || park.fullName}
-            className="h-48 w-full object-cover"/>
-            <div className="p-5">
-              <p className="text-stone-500 text-sm mb-2">{park.designation}</p>
-              <h2 className="text-lg font-semibold text-[#2F4F3A] mb-2">
-                {park.fullName}
-              </h2>
-              <p className="text-sm text-stone-600 line-clamp-3">
-                {park.description}
-              </p>
+
+            <div className="bg-white rounded-2xl border border-stone-200 shadow-sm overflow-hidden hover:shadow-lg transition">
+              <Link key={park.id} to={`/parks/${park.parkCode}`}>
+                <img src={park.images?.[0]?.url} alt={park.images?.[0]?.altText || park.fullName}
+                className="h-48 w-full object-cover"/>
+                <div className="p-5">
+                  <p className="text-stone-500 text-sm mb-2">{park.designation}</p>
+                  <h2 className="text-lg font-semibold text-[#2F4F3A] mb-2">
+                    {park.fullName}
+                  </h2>
+                  <p className="text-sm text-stone-600 line-clamp-3">
+                    {park.description}
+                  </p>
+                </div>
+              </Link>
+              <button 
+                className=" w-50 m-2 rounded-full bg-stone-500 text-white shadow-sm hover:bg-stone-600 transition cursor-pointer"
+                >
+                  Add to Planner
+              </button>
             </div>
-            </Link>
+
           ))}
       </div>
     </section>
